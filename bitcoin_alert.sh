46c2edcecbbf8ec4101978ecffe837c20fffd47d
Ksnovaes <kauanovais84@gmail.com>
2025-11-11 12:14:35 -0300

    tarefa 02 - adicionando argumentos para a consulta

--- bitcoin_alert.sh
+++ bitcoin_alert.sh
@@ -1,9 +1,10 @@
 API_URL="https://cointradermonitor.com/api/pbb/v1/ticker" 
+VALOR_ATUALIZADO=$1
 
 value=$(curl -s $API_URL | jq '.last')
 
 echo "Preço atual do Bitcoin: $value"
 
-if [ $(echo "$value < 560000" | bc) -eq 1 ]; then
-    echo "Alerta: Valor do Bitcoin caiu abaixo de 560.000"
+if [ $(echo "$value < $VALOR_ATUALIZADO" | bc) -eq 1 ]; then
+    echo "Alerta: Valor do Bitcoin caiu abaixo de $VALOR_ATUALIZADO"
 fi
