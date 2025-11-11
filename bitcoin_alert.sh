9afacec862358ebc5b2c492fa913fe75e2647dc9
Ksnovaes <kauanovais84@gmail.com>
2025-11-11 13:26:48 -0300

    pegando o horário correto

--- bitcoin_alert.sh
+++ bitcoin_alert.sh
@@ -7,9 +7,8 @@ echo "Preço atual do Bitcoin: $value"
 
 if [ $(echo "$value < $VALOR_ATUALIZADO" | bc) -eq 1 ]; then
     echo "Alerta: Valor do Bitcoin caiu abaixo de $VALOR_ATUALIZADO"
-    export TZ="America/Sao_Paulo"
     echo "value=$value" >> $GITHUB_ENV
-    echo "datetime=$(date -u +'%d/%m/%Y %H:%M')" >> $GITHUB_ENV
+    echo "datetime=$(TZ='America/Sao_Paulo' date +'%d/%m/%Y %H:%M:%S')" >> $GITHUB_ENV
     exit 2
 else
     echo "O valor do Bitcoin está acima de $VALOR_ATUALIZADO"
